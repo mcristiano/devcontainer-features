9d355827543e70ddce9325dac26056c5f21edc32
mcristiano <36131606+mcristiano@users.noreply.github.com>
2026-03-16 09:29:24 -0300

    Refactor access rights commands in create.sh
    Updated ownership and permissions commands for workspace.

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -6,5 +6,9 @@ chmod 700 /home/vscode/.ssh
 ssh-keyscan github.com >>/home/vscode/.ssh/known_hosts
 
 ### Ensure correct access rights
-sudo chown -Rf vscode:vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
-sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+WORKSPACE="${WORKSPACE_FOLDER:-/workspaces}"
+sudo chown -Rf vscode:vscode "$WORKSPACE"
+sudo chmod -Rf 755 "$WORKSPACE"
+
+#sudo chown -Rf vscode:vscode ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
+#sudo chmod -Rf 755 ${containerWorkspaceFolder:-.}/* ${containerWorkspaceFolder:-.}/.*
